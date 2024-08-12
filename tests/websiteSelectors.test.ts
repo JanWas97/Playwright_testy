a0220c4956f0a84da70be4e9123c7adf6320d00b
JA <chwilowy97@gmail.com>
2024-08-12 16:16:40 +0200

    Created new script

--- tests/websiteSelectors.test.ts
+++ tests/websiteSelectors.test.ts
@@ -1,6 +1,6 @@
 import { click } from 'C:/Users/JA/Documents/Visual_studio/Playwright_testy/utilities/elementgetnclick';
 import { test } from '@playwright/test';
-test.only('Website testing', async ({ page })  => { 
+test('Website testing', async ({ page })  => { 
 await page.goto('https://arstechnica.com/');
 await click(page, 'Cleared cookies', '#onetrust-accept-btn-handler');
 await page.waitForTimeout(2000);

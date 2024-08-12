8951a42cb7f813e29d1eeb579146d57908856bf6
JA <chwilowy97@gmail.com>
2024-08-12 16:10:02 +0200

    Created new script

---
+++ tests/websiteSelectors.test.ts
@@ -0,0 +1,7 @@
+import { click } from 'C:/Users/JA/Documents/Visual_studio/Playwright_testy/utilities/elementgetnclick';
+import { test } from '@playwright/test';
+test.only('Website testing', async ({ page })  => { 
+await page.goto('https://arstechnica.com/');
+await click(page, 'Cleared cookies', '#onetrust-accept-btn-handler');
+await page.waitForTimeout(2000);
+})

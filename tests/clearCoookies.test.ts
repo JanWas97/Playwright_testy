0a99e529d6023464c74f70e1ad3ec4efa328e6a3
JA <chwilowy97@gmail.com>
2025-02-27 14:54:43 +0100

    Changes: cleaning up

--- tests/clearCoookies.test.ts
+++ tests/clearCoookies.test.ts
@@ -3,7 +3,6 @@ import { test } from '@playwright/test';
 test('nowa konfiguracja', async ({ page })  => { 
 await page.goto('https://www.morele.net/');
 await click(page, 'dismissed cookies', '#cookie-consent > dialog > div > div > div.actions > button.btn.btn-primary.w-100.btn--save-all');
-await page.waitForTimeout(3000);
 })
 
 
--- tests/hoverOver.test.ts
+++ tests/hoverOver.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { click } from '../utilities/elementgetnclick';
 import { existsOrDefault } from '../utilities/checkingifexist';
-test('I dont know what it does', async ({ page })  => { 
+test('hovering test', async ({ page })  => { 
 await page.goto('https://developer.chrome.com/');
 await page.hover('a[href="https://developer.chrome.com/docs"]');
 await click(page,'Clicked button from drop down menu', 'a[href="https://developer.chrome.com/docs/chromium"]');
